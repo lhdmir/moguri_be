718f404fd3330233f2831cf712b5505b8e63969c
lhdmir <lhdmir@naver.com>
2024-07-27 23:22:20 +0900

    프로젝트 생성

---
+++ src/main/java/ync/likelion/moguri_be/MoguriBeApplication.java
@@ -0,0 +1,13 @@
+package ync.likelion.moguri_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoguriBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoguriBeApplication.class, args);
+    }
+
+}
