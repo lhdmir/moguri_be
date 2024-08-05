612740ffa28247af159739fb3f2b6bf7a1447ccd
JinsuBae2 <bjs7898@naver.com>
2024-08-05 11:43:01 +0900

    swagger 어노테이션 추가

--- src/main/java/ync/likelion/moguri_be/controller/ItemController.java
+++ src/main/java/ync/likelion/moguri_be/controller/ItemController.java
@@ -1,5 +1,8 @@
 package ync.likelion.moguri_be.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,11 +32,23 @@ public class ItemController {
     private ItemService itemService;
 
     @PostMapping("/accessory")
+    @Operation(summary = "액세서리 뽑기", description = "사용자가 액세서리를 뽑습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "액세서리가 성공적으로 뽑혔습니다."),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자"),
+            @ApiResponse(responseCode = "409", description = "모든 아이템을 소유하고 있음")
+    })
     public ResponseEntity<Object> drawAccessory(@RequestHeader("Authorization") String authorization) {
         return drawItem(authorization, true, "Accessory drawn successfully");
     }
 
     @PostMapping("/background")
+    @Operation(summary = "배경화면 뽑기", description = "사용자가 배경화면 뽑습니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "배경화면이 성공적으로 뽑혔습니다."),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자"),
+            @ApiResponse(responseCode = "409", description = "모든 아이템을 소유하고 있음")
+    })
     public ResponseEntity<Object> drawBackground(@RequestHeader("Authorization") String authorization) {
         return drawItem(authorization, false, "Background drawn successfully");
     }
